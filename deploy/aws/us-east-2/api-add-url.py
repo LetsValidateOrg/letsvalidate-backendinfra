6d574bb06eeb783ffca7c26783c9c2b261b2007d
Terry Ott <Terry.Ott@publicntp.org>
2023-08-08 00:08:06 +0000

    connecting to DB using params from SSM now

--- deploy/aws/us-east-2/api-add-url.py
+++ deploy/aws/us-east-2/api-add-url.py
@@ -4,6 +4,7 @@ import logging
 import json
 import boto3
 import botocore.exceptions
+import psycopg
 
 logger = logging.getLogger()
 logger.setLevel( logging.DEBUG ) 
@@ -38,12 +39,60 @@ def letsvalidate_api_add_url(event, context):
 
     logger.info(f"Cognito user w/ ID {user_cognito_id} requested to monitor URL \"{url_to_monitor}\"")
 
+    existing_url_info = _get_existing_url_info(url_to_monitor)
+
+    if existing_url_info is not None:
+        logger.info("Found existing info about this URL")
+
     body = {
         "requested_url": url_to_monitor,
     }
     status_code = 200
 
     return _create_lambda_response(status_code, body, headers)
+
+
+def _get_existing_url_info(url_to_monitor):
+    with _get_db_handle() as dbHandle:
+        with dbHandle.cursor() as dbCursor:
+            pass
+
+
+def _get_db_handle():
+    db_params = _get_ssm_db_parameters()
+    logger.debug("DB params:")
+    logger.debug(json.dumps(db_params, indent=4, sort_keys=True))
+
+    return psycopg.connect(
+        host        = db_params['dbhost'],
+        user        = db_params['dbuser'],
+        password    = db_params['dbpassword'],
+        dbname      = db_params['dbname'] )
+     
+
+def _get_ssm_params( param_list ):
+    returned_parameters = boto3_clients['ssm'].get_parameters( Names=param_list )
+
+    ssm_params = {}
+
+    for curr_param in returned_parameters['Parameters']:
+        # Final component after slash will be key
+        param_name = curr_param['Name'].split('/')[-1]
+        ssm_params[param_name] = curr_param['Value']
+
+    return ssm_params
+
+
+def _get_ssm_db_parameters():
+
+    param_store_keys = (
+        "/letsvalidate/db/aws/us-east-2/dbname",
+        "/letsvalidate/db/aws/us-east-2/dbhost",
+        "/letsvalidate/db/aws/us-east-2/dbuser",
+        "/letsvalidate/db/aws/us-east-2/dbpassword",
+    )
+
+    return _get_ssm_params( param_store_keys )
 
 
 def _create_lambda_response( status_code, body, headers ):
--- letsvalidate.sql
+++ letsvalidate.sql
@@ -1,8 +1,20 @@
 DROP TABLE monitored_urls;
+DROP TABLE urls;
 
-CREATE TABLE monitored_urls (
+CREATE TABLE urls (
     url_id_pk   UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
     url         VARCHAR     NOT NULL UNIQUE
 );
 
-CREATE INDEX idx_monitored_urls_url ON monitored_urls (url);
+CREATE INDEX idx_urls_url ON urls (url);
+
+CREATE TABLE monitored_urls (
+    monitor_id_pk   UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+    url_id          UUID        NOT NULL REFERENCES urls(url_id_pk),
+    cognito_user_id UUID        NOT NULL,
+
+    UNIQUE (url_id, cognito_user_id)
+);
+
+CREATE INDEX idx_monitored_urls_url ON monitored_urls (url_id);
+CREATE INDEX idx_monitored_urls_user ON monitored_urls (cognito_user_id);
