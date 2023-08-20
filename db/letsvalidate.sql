e8056e5a8686bc037288d886587921a58286bb6b
Terry Ott <Terry.Ott@publicntp.org>
2023-08-20 18:59:57 +0000

    Overdue checkin

---
+++ db/letsvalidate.sql
@@ -0,0 +1,36 @@
+DROP TABLE monitored_urls CASCADE;
+DROP TABLE cert_issuers CASCADE;
+DROP TABLE urls;
+
+CREATE TABLE cert_issuers (
+    cert_issuer_id_pk       UUID            PRIMARY KEY DEFAULT gen_random_uuid(),
+    cert_issuer             varchar         NOT NULL UNIQUE );
+
+CREATE INDEX idx_cert_issuers_issuer ON cert_issuers(cert_issuer);
+
+
+CREATE TABLE urls (
+    url_id_pk               UUID            PRIMARY KEY DEFAULT gen_random_uuid(),
+    url                     VARCHAR         NOT NULL UNIQUE,
+    cert_retrieved          timestamp with time zone,
+    cipher_suite            integer,
+    cert_issuer             UUID            REFERENCES cert_issuers(cert_issuer_id_pk) ON DELETE CASCADE,
+    cert_subject            varchar,
+    cert_not_valid_before   timestamp with time zone,
+    cert_not_valid_after    timestamp with time zone
+
+);
+
+CREATE INDEX idx_urls_url ON urls (url);
+
+CREATE TABLE monitored_urls (
+    monitor_id_pk   UUID        PRIMARY KEY DEFAULT gen_random_uuid(),
+    monitor_added   timestamp with time zone NOT NULL,
+    url_id          UUID        NOT NULL REFERENCES urls(url_id_pk) ON DELETE CASCADE,
+    cognito_user_id UUID        NOT NULL,
+
+    UNIQUE (url_id, cognito_user_id)
+);
+
+CREATE INDEX idx_monitored_urls_url ON monitored_urls (url_id);
+CREATE INDEX idx_monitored_urls_user ON monitored_urls (cognito_user_id);
--- deploy/aws/us-east-2/api-add-url.py
+++ deploy/aws/us-east-2/api-add-url.py
@@ -5,6 +5,10 @@ import json
 import boto3
 import botocore.exceptions
 import psycopg
+import ssl
+import OpenSSL
+import datetime
+
 
 logger = logging.getLogger()
 logger.setLevel( logging.DEBUG ) 
@@ -35,27 +39,89 @@ def letsvalidate_api_add_url(event, context):
 
     url_to_monitor = event['queryStringParameters']['url']
 
-    user_cognito_id = event['requestContext']['authorizer']['jwt']['claims']['sub']
+    user_cognito_id = _get_cognito_user_id(event)
 
     logger.info(f"Cognito user w/ ID {user_cognito_id} requested to monitor URL \"{url_to_monitor}\"")
 
-    existing_url_info = _get_existing_url_info(url_to_monitor)
+    with _get_db_handle() as db_handle:
+        with db_handle.cursor() as db_cursor:
 
-    if existing_url_info is not None:
-        logger.info("Found existing info about this URL")
+            existing_url_info = _get_existing_url_info(db_cursor, url_to_monitor)
 
-    body = {
-        "requested_url": url_to_monitor,
-    }
-    status_code = 200
+            if existing_url_info is not None:
+                logger.info("Found existing info about this URL")
+                # Is THIS user already monitoring it?
+                if _user_already_monitoring(db_cursor, user_cognito_id, url_to_monitor):
+                    logger.debug("User already monitoring this URL")
+                    status_code = 204
+                    body = None
+                else:
+                    logger.debug("User not monitoring this URL")
+                    # Add a monitor for this user, return them the data from last pull
+                    _add_monitor_for_user(db_cursor, 
+                        user_cognito_id, url_to_monitor)
+             
+                    body = {
+                        "cert_retrieved": existing_url_info[0].isoformat(),
+                        "not_before": existing_url_info[1].isoformat(),
+                        "not_after": existing_url_info[2].isoformat(),
+                    }
+
+                    status_code = 200
+            else:
+                logger.info("Found no existing data about this URL")
+
+                # Pull its tls cert
+                try:
+                    cert_info = _do_tls_handshake(url_to_monitor)
+
+                    # Add all database rows
+                    _add_new_cert_with_monitor( db_cursor, url_to_monitor, cert_info, 
+                        user_cognito_id )
+                except Exception as e:
+                    logger.warn("Unable to pull cert for URL: " + str(e))
+
+                body = {
+                    "status": "no existing data",
+                }
+
+                status_code = 200
 
     return _create_lambda_response(status_code, body, headers)
 
 
-def _get_existing_url_info(url_to_monitor):
-    with _get_db_handle() as dbHandle:
-        with dbHandle.cursor() as dbCursor:
-            pass
+def _user_already_monitoring(db_cursor, user_id, url):
+    db_cursor.execute("""
+        SELECT  monitor_id_pk
+        FROM    monitored_urls
+        JOIN    urls ON monitored_urls.url_id = urls.url_id_pk
+        WHERE   monitored_urls.cognito_user_id = %s AND urls.url = %s;""",
+
+        (user_id, url) )
+
+    return db_cursor.fetchone() is not None
+
+
+def _add_monitor_for_user(db_cursor, user_id, url):
+    db_cursor.execute("""
+        INSERT INTO monitored_urls (monitor_added, url_id, cognito_user_id) 
+        VALUES ( 
+            NOW(),
+            (SELECT url_id_pk 
+             FROM   urls
+             WHERE  url = %s),
+            %s );""",
+        (url, user_id) )
+
+
+def _get_existing_url_info(db_cursor, url_to_monitor):
+    db_cursor.execute("""
+        SELECT      cert_retrieved, cert_not_valid_before, cert_not_valid_after
+        FROM        urls
+        WHERE       url = %s;
+        """, (url_to_monitor,) )
+        
+    return db_cursor.fetchone()
 
 
 def _get_db_handle():
@@ -97,7 +163,7 @@ def _get_ssm_db_parameters():
 
 def _create_lambda_response( status_code, body, headers ):
     if body is not None:
-        body_text = json.dumps(body, indent=4, sort_keys=True )
+        body_text = json.dumps(body, indent=4, sort_keys=True, default=str )
     else:
         body_text = None
 
@@ -111,3 +177,81 @@ def _create_lambda_response( status_code, body, headers ):
 
     return response
 
+
+def _get_cognito_user_id(event):
+    return event['requestContext']['authorizer']['jwt']['claims']['sub']
+
+def _do_tls_handshake(url):
+    server_cert = ssl.get_server_certificate( ('letsvalidate.org', 443) )
+
+    # Decode the cert
+    parsed_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM,
+        str.encode(server_cert))
+    #logger.info("Parsed server cert:")
+    not_before = _parse_x509_date( parsed_cert.get_notBefore().decode() )
+    not_after = _parse_x509_date( parsed_cert.get_notAfter().decode() )
+    issuer = parsed_cert.get_issuer().organizationName
+    subject = parsed_cert.get_subject().commonName
+
+    cert_info = {
+        "issuer"        : issuer,
+        "subject"       : subject,
+        "not_before"    : not_before,
+        "not_after"     : not_after,
+    }
+
+    logger.info(json.dumps(cert_info, indent=4, sort_keys=True, default=str))
+    return cert_info
+
+def _parse_x509_date(date_string):
+
+    no_timezone = datetime.datetime.strptime( date_string, '%Y%m%d%H%M%SZ' )
+
+    # Set to UTC timezone
+    with_timezone = no_timezone.replace(tzinfo=datetime.timezone.utc)
+
+    return with_timezone
+
+
+def _add_new_cert_with_monitor(db_cursor, url, cert_info, user_cognito_id ):
+    # Let's see if this cert issuer is known
+    db_cursor.execute("""
+        SELECT cert_issuer_id_pk 
+        FROM cert_issuers
+        WHERE cert_issuer = %s;""",
+
+        (cert_info['issuer'],) )
+
+    cert_issuer_row = db_cursor.fetchone()
+
+    if cert_issuer_row is not None:
+        cert_issuer_id = cert_issuer_row[0]
+    else:
+        db_cursor.execute("""
+            INSERT INTO cert_issuers (cert_issuer)
+            VALUES ( %s )
+            RETURNING cert_issuer_id_pk;""",
+
+            (cert_info['issuer'],) )
+
+        cert_issuer_id = db_cursor.fetchone()[0]
+
+    db_cursor.execute("""
+        INSERT INTO urls (url, cert_retrieved, cert_issuer, cert_subject, 
+            cert_not_valid_before, cert_not_valid_after)
+        VALUES ( %s, NOW(), %s, %s, %s, %s )
+        RETURNING url_id_pk;""",
+
+        (url, cert_issuer_id, cert_info['subject'], cert_info['not_before'],
+            cert_info['not_after']) )
+
+    # Get the ID for that new row
+    new_url_id = db_cursor.fetchone()[0]
+
+    db_cursor.execute("""
+        INSERT INTO monitored_urls ( monitor_added, url_id, cognito_user_id )
+        VALUES ( NOW(), %s, %s );""",
+
+        (new_url_id, user_cognito_id) )
+    
+        
