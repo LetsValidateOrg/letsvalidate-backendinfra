4cf33eab5a16c44fe16e05bbca892ce9c252a48c
Terry Ott <Terry.Ott@publicntp.org>
2023-09-03 21:24:28 +0000

    Parsing params for monitor_id to delete

--- deploy/aws/us-east-2/api-remove-url.py
+++ deploy/aws/us-east-2/api-remove-url.py
@@ -4,6 +4,7 @@ import logging
 import letsvalidate.util.aws_apigw
 import boto3
 import json
+import uuid
 
 
 logger = logging.getLogger( "letsvalidate" )
@@ -17,6 +18,20 @@ boto3_clients = {
 }
 
 
+def _validate_query_string_params(event, headers):
+    if 'queryStringParameters' not in event or 'monitor_id' not in event['queryStringParameters']:
+        return None
+
+    # Make sure it has a proper value
+    try:
+        monitor_id = uuid.UUID(event['queryStringParameters']['monitor_id'])
+    except ValueError:
+        logger.warn("Monitor ID value of {event['queryStringParameters']['monitor_id']} was not a valid UUID")
+        return None
+
+    return str(monitor_id)
+
+
 def letsvalidate_api_remove_url(event, context):
     logger.debug( "Incoming event" )
     logger.debug( json.dumps(event, indent=4, sort_keys=True) )
@@ -24,6 +39,17 @@ def letsvalidate_api_remove_url(event, context):
     headers = {
         "content-type": "application/json",
     }
+
+    monitor_id_to_delete = _validate_query_string_params(event, headers)
+
+    if monitor_id_to_delete is None:
+        status_code = 400
+
+        body = {
+            "error": "URL to did not include valid \"monitor_id\" URL query parameter"
+        }
+
+        return letsvalidate.util.aws_apigw.create_lambda_response( status_code, body, headers )
 
     body = None
     status_code = 501
