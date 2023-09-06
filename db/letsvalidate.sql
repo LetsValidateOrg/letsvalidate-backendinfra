1fa241f29a98e065fac099254c35619c93252648
Terry Ott <Terry.Ott@publicntp.org>
2023-09-06 22:24:11 +0000

    Recording more details about certs

--- db/letsvalidate.sql
+++ db/letsvalidate.sql
@@ -5,27 +5,45 @@ DROP TABLE urls;
 CREATE TABLE cert_issuers (
     cert_issuer_id_pk       UUID            PRIMARY KEY DEFAULT gen_random_uuid(),
 
-    common_name             varchar         NOT NULL,
-    country                 varchar,
-    organization            varchar,
-    organizational_unit     varchar,
+    common_name                 varchar         NOT NULL,
+    country_name                varchar,
+    email_address               varchar,
+    locality_name               varchar,
+    organization_name           varchar,
+    organizational_unit_name    varchar,
+    state_or_province           varchar,
 
-    UNIQUE (common_name, country, organization, organizational_unit)
+    UNIQUE (common_name, country_name, email_address, locality_name, organization_name,
+        organizational_unit_name, state_or_province)
 );
 
 CREATE INDEX cert_issuers_idx_common_name ON cert_issuers(common_name);
 
 
 CREATE TABLE urls (
-    url_id_pk                   UUID            PRIMARY KEY DEFAULT gen_random_uuid(),
-    url                         VARCHAR                     NOT NULL UNIQUE,
-    cert_retrieved              timestamp with time zone,
-    cipher_suite                integer,
-    cert_issuer                 UUID            REFERENCES cert_issuers(cert_issuer_id_pk) ON DELETE CASCADE NOT NULL,
-    cert_subject_common_name    varchar                     NOT NULL UNIQUE,
-    cert_not_valid_before       timestamp with time zone,
-    cert_not_valid_after        timestamp with time zone
+    url_id_pk                               UUID            PRIMARY KEY DEFAULT gen_random_uuid(),
+    url                                     VARCHAR NOT NULL UNIQUE,
+    cert_retrieved                          timestamp with time zone        NOT NULL,
+    cipher_suite                            integer,
 
+    cert_issuer                             UUID            REFERENCES cert_issuers(cert_issuer_id_pk) ON DELETE CASCADE NOT NULL,
+
+    cert_subject_common_name                varchar                         NOT NULL UNIQUE,
+    cert_subject_country_name               varchar,
+    cert_subject_email_address              varchar,
+    cert_subject_locality_name              varchar,
+    cert_subject_organization_name          varchar,
+    cert_subject_organizational_unit_name   varchar,
+    cert_subject_state_or_province          varchar,
+
+    cert_not_valid_before                   timestamp with time zone        NOT NULL,
+    cert_not_valid_after                    timestamp with time zone        NOT NULL,
+
+    serial_number                           integer,
+    cert_version                            integer,
+
+    -- Will never have a duplicate entry for common name & version tuple 
+    UNIQUE (cert_subject_common_name, cert_version)
 );
 
 CREATE INDEX urls_idx_url               ON urls (url);
--- deploy/aws/us-east-2/api-add-url.py
+++ deploy/aws/us-east-2/api-add-url.py
@@ -75,6 +75,7 @@ def letsvalidate_api_add_url(event, context):
                         user_cognito_id )
         
                 except Exception as e:
+                    raise e
                     logger.warn("Unable to pull cert for URL: " + str(e))
 
                     status_code = 400
@@ -142,6 +143,50 @@ def _get_cognito_user_id(event):
     return event['requestContext']['authorizer']['jwt']['claims']['sub']
 
 
+def parseX509Name(x509Name):
+
+    # Info from: https://www.pyopenssl.org/en/latest/api/crypto.html#x509name-objects
+
+    name_components = x509Name.get_components
+
+    # Initialize so that all keys exist
+    parsed_x509_name = {}
+
+    x509_field_code_to_hash_code = {
+        "C"     : 'countryName',
+        "ST"    : 'stateOrProvinceName',
+        "L"     : 'localityName',
+        "O"     : 'organizationName',
+        "OU"    : 'organizationalUnitName',
+        "CN"    : 'commonName',
+
+        # Apparently email has two mappings? 
+        #    https://stackoverflow.com/a/42549270
+        "E"             : 'emailAddress',
+        "EMAILADDRESS"  : "emailAddress",
+    }
+
+    for expandedName in x509_field_code_to_hash_code.values():
+        if expandedName not in parsed_x509_name:
+            parsed_x509_name[ expandedName ] = None
+
+    for curr_component_entry in x509Name.get_components():
+        component_key       = curr_component_entry[0].decode()
+        component_value     = curr_component_entry[1].decode()
+
+        if component_key not in x509_field_code_to_hash_code:
+            logger.warn("Got unknown x509 field in cert name: {component_key}, skipping")
+            continue
+
+        mapped_key = x509_field_code_to_hash_code[component_key]
+
+        logger.debug(f"Got component entry ({component_key} => {mapped_key}, {component_value})")
+
+        parsed_x509_name[mapped_key] = component_value
+        
+    return parsed_x509_name
+
+
 def _do_tls_handshake(url):
     # Get the hostname and port out of a URL
     parsed_uri = urllib.parse.urlparse(url)
@@ -159,16 +204,26 @@ def _do_tls_handshake(url):
     parsed_cert = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM,
         str.encode(server_cert))
     #logger.info("Parsed server cert:")
-    not_before = _parse_x509_date( parsed_cert.get_notBefore().decode() )
-    not_after = _parse_x509_date( parsed_cert.get_notAfter().decode() )
-    issuer = parsed_cert.get_issuer().organizationName
-    subject = parsed_cert.get_subject().commonName
+
+    # https://www.pyopenssl.org/en/latest/api/crypto.html#x509-objects
+    not_before                  = _parse_x509_date( parsed_cert.get_notBefore().decode() )
+    not_after                   = _parse_x509_date( parsed_cert.get_notAfter().decode() )
+
+    subject_info                = parseX509Name( parsed_cert.get_subject() )
+    issuer_info                 = parseX509Name( parsed_cert.get_issuer() )
 
     cert_info = {
-        "issuer"        : issuer,
-        "subject"       : subject,
-        "not_before"    : not_before,
-        "not_after"     : not_after,
+        "subject_info"          : subject_info,
+        "issuer_info"           : issuer_info,
+        "validity_range" : {
+            "not_before"        : not_before,
+            "not_after"         : not_after,
+        },
+        
+        # Add this back in at some point when we find out why it's so busted
+        # "serial_number"         : parsed_cert.get_serial_number(),
+        "serial_number"         : None,
+        "cert_version"          : parsed_cert.get_version(),
     }
 
     logger.info(json.dumps(cert_info, indent=4, sort_keys=True, default=str))
@@ -186,42 +241,78 @@ def _parse_x509_date(date_string):
 
 
 def _add_new_cert_with_monitor(db_cursor, url, cert_info, user_cognito_id ):
+    logging.debug(f"Checking if we have an entry for this cert issuer: {cert_info['issuer_info']['commonName']}")
     # Let's see if this cert issuer is known
     db_cursor.execute("""
         SELECT cert_issuer_id_pk 
         FROM cert_issuers
-        WHERE cert_issuer = %s;""",
+        WHERE common_name = %s;""",
 
-        (cert_info['issuer'],) )
+        (cert_info['issuer_info']['commonName'],) )
 
     cert_issuer_row = db_cursor.fetchone()
 
     if cert_issuer_row is not None:
+        logging.debug(f"Found issuing cert with common name {{cert_info['issuer_info']['commonName']}}, returning the ID for the entry in the DB")
         cert_issuer_id = cert_issuer_row[0]
     else:
+        logging.debug(f"No existing cert matching common name {cert_info['issuer_info']['commonName']}, adding new row")
+        cert_issuer_info = cert_info['issuer_info']
+        logging.debug("Issuer info")
+        logging.debug(json.dumps(cert_issuer_info, indent=4, sort_keys=True) )
         db_cursor.execute("""
-            INSERT INTO cert_issuers (cert_issuer)
-            VALUES ( %s )
+            INSERT INTO cert_issuers (common_name, country_name, email_address, locality_name,
+                organization_name, organizational_unit_name, state_or_province )
+            VALUES ( %s, %s, %s, %s, %s, %s, %s )
             RETURNING cert_issuer_id_pk;""",
 
-            (cert_info['issuer'],) )
+            (cert_issuer_info['commonName'], cert_issuer_info['countryName'], cert_issuer_info['emailAddress'],
+                cert_issuer_info['localityName'], cert_issuer_info['organizationName'], 
+                cert_issuer_info['organizationalUnitName'], cert_issuer_info['stateOrProvinceName']))
 
         cert_issuer_id = db_cursor.fetchone()[0]
 
+        logging.debug(f"New cert issuer ID: {cert_issuer_id}")
+
+    cert_subject_info = cert_info['subject_info']
+
+    logging.debug("Cert subject info")
+    logging.debug(json.dumps(cert_subject_info, indent=4, sort_keys=True))
+
     db_cursor.execute("""
-        INSERT INTO urls (url, cert_retrieved, cert_issuer, cert_subject, 
-            cert_not_valid_before, cert_not_valid_after)
-        VALUES ( %s, NOW(), %s, %s, %s, %s )
+        INSERT INTO urls (url, cert_retrieved, cert_issuer, 
+
+            cert_subject_common_name, cert_subject_country_name, cert_subject_email_address, 
+            cert_subject_locality_name, cert_subject_organization_name, 
+            cert_subject_organizational_unit_name, cert_subject_state_or_province,
+
+            cert_not_valid_before, cert_not_valid_after,
+
+            serial_number, cert_version)
+        VALUES ( %s, NOW(), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s )
         RETURNING url_id_pk;""",
 
-        (url, cert_issuer_id, cert_info['subject'], cert_info['not_before'],
-            cert_info['not_after']) )
+        (url, cert_issuer_id, 
+
+            cert_subject_info['commonName'], cert_subject_info['countryName'], 
+            cert_subject_info['emailAddress'], cert_subject_info['localityName'],
+            cert_subject_info['organizationName'], cert_subject_info['organizationalUnitName'],
+            cert_subject_info['stateOrProvinceName'],
+
+            cert_info['validity_range']['not_before'], cert_info['validity_range']['not_after'],
+
+            cert_info['serial_number'], cert_info['cert_version']) 
+    )
 
     # Get the ID for that new row
     new_url_id = db_cursor.fetchone()[0]
+
+    logging.debug(f"New URL ID: {new_url_id}")
 
     db_cursor.execute("""
         INSERT INTO monitored_urls ( monitor_added, url_id, cognito_user_id )
         VALUES ( NOW(), %s, %s );""",
 
         (new_url_id, user_cognito_id) )
+
+    logging.debug("Successfully added entry to monitored_urls")
